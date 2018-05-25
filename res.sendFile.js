8b904086ceaf154e0eddae080756e557922d2029
Khamel <khamel.mumford@gmail.com>
2018-05-25 05:33:40 -0500

    iniital push

---
+++ res.sendFile.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === "production") {
+    // Serve up static assets (usually on heroku)
+    app.use(express.static(__dirname + 'public'))
+}
+else {
+    // Serve up static assets (usually on heroku)
+    app.use(express.static('public'))
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+    res.sendfile(path.join(__dirname, "public/index.html"))
+});
+
+
+app.listen(PORT, function () {
+    console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
